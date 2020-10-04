8d67c834fb13190d2df02c7b0677b7daded2a35e
Jesse <jesseconner@visualpurge.com>
2020-10-04 10:02:58 -0400

    Add some more descriptive output and don't try to write unknown file types

--- tool.py
+++ tool.py
@@ -6,15 +6,17 @@ text=""
 with open("sample.html","r") as f:
 	text=f.read()
 
-print(len(text))
-#objects=re.findall(r'/data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%]*\s*/', text)
-#objects=re.findall(r'data:[a-z]+\/[a-z]+;[a-z\-]+\=[a-z\-]?;base64?,[a-z0-9\!\$\&\'\,\*\+\,\;\=\-\.\_\~\:\@\/\?\%]*[\s\)]+', text)
+print("File is " + str(len(text)) + " characters")
+
 objects=re.findall(r'(data:[a-z]+\/[a-z\+]+;base64,)([a-zA-Z0-9\!\$\&\'\,\*\+\,\;\=\-\.\_\~\:\@\/\?\%]*)[\s\)\"]+', text)
-#[a-z0-9\!\$\&\'\,\*\+\,\;\=\-\.\_\~\:\@\/\?\%]*[\s\)]+
-print(len(objects))
+
+print("Found " + str(len(objects)) + " encoded objects")
+
 x=0
 for f in objects:
 	x=x+1
+
+	print("Writing dat file for file " + str(x))
 	with open("output/file" + str(x) + ".dat","w") as newFile:
 		newFile.write(f[0] + f[1])
 	
@@ -25,6 +27,11 @@ for f in objects:
 	elif("svg" in f[0]):
 		type="svg"
 
-	with open("output/image" + str(x) + "." + type,"wb") as newFile:
-		encodedString=f[1].encode()
-		newFile.write(base64.decodebytes(encodedString))
+	if type != "":
+
+		path="output/image" + str(x) + "." + type
+		print("Known file type. Writing " + path)
+
+		with open(path,"wb") as newFile:
+			encodedString=f[1].encode()
+			newFile.write(base64.decodebytes(encodedString))
